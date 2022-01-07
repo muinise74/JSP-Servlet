3355752a526633edc84c804f424bb91d4bd0baec
muinise74 <95010730+muinise74@users.noreply.github.com>
2022-01-07 09:51:13 +0900

    Update JoinService.java

--- JoinService.java
+++ JoinService.java
@@ -6,17 +6,30 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.smhrd.model.S_MemberDAO;
+import com.smhrd.model.S_MemberDTO;
 
 @WebServlet("/JoinService")
 public class JoinService extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
+	S_MemberDAO dao = new S_MemberDAO();
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		request.setCharacterEncoding("UTF-8");
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
 		String nick = request.getParameter("nick");
+
+		S_MemberDTO member = new S_MemberDTO(id, pw, nick);
+		int check = dao.memberJoin(member);
+		if (check > 0) {
+			response.sendRedirect("joinTrue.jsp");
+		} else {
+			response.sendRedirect("joinFalse.jsp");
+		}
 	}
 
 }
